008b3573b0522bac02636ebf2ab4736baccf5414
pc <pc@DESKTOP-BI5FP4B>
2021-06-16 16:58:58 +0200

    EOF value

--- hello/main.c
+++ hello/main.c
@@ -12,20 +12,20 @@ void temperature_tables_while()
 	fahr = lower;
 	printf("##########################\n");
 	printf("#####FAHR to CELSIUS######\n");
-    printf("##########################\n");
-	while(fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr-32.0);
+	printf("##########################\n");
+	while (fahr <= upper) {
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
 		printf("%7.0f %10.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
-	
+
 
 	celsius = lower;
 	printf("##########################\n");
 	printf("#####CELSIUS to FAHR######\n");
-    printf("##########################\n");
-	while(celsius <= upper) {
-		fahr = (celsius * 9.0/5.0) + 32;
+	printf("##########################\n");
+	while (celsius <= upper) {
+		fahr = (celsius * 9.0 / 5.0) + 32;
 		printf("%7.1f %10.0f\n", celsius, fahr);
 		celsius = celsius + step;
 	}
@@ -33,20 +33,33 @@ void temperature_tables_while()
 
 void temperature_tables_for()
 {
+#define		LOWER 0    /* lower limit of table*/
+#define		UPPER 300 /*upper limit*/
+#define		STEP 20   /*step size*/
+
 	int fahr;
+
 	printf("\n\n\ From 0 to 300\n");
 
-	for(fahr = 0; fahr <= 300; fahr = fahr + 20)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
 	printf("\n\n\ From 300 to 0\n");
 
-	for(fahr = 300; fahr >= 0; fahr = fahr - 20)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	for (fahr = UPPER; fahr >= 0; fahr = fahr - STEP)
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+}
+
+void input_output()
+{
+	
+	printf("\nThe EOF value is: %d", EOF);
+
 }
 
 void main()
 {
 	temperature_tables_while();
 	temperature_tables_for();
+	input_output();
 }
